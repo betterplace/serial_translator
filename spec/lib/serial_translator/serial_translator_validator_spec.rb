7d86f0f873f06789d9686106009eb02619697ea4
Johannes Opper <johannes.opper@gmail.com>
2014-09-20 10:49:18 +0200

    New rspec syntax, hurray

--- spec/lib/serial_translator/serial_translator_validator_spec.rb
+++ spec/lib/serial_translator/serial_translator_validator_spec.rb
@@ -6,33 +6,33 @@ describe SerialTranslator::SerialTranslatorValidator do
   describe 'presence validation' do
     it 'validates presence correctly if attribute is nil' do
       example.valid?
-      example.errors[:description].size.should eq 1
+      expect(example.errors[:description].size).to eq 1
     end
 
     it 'validates presence correctly if attribute is empty' do
       example.description = ''
       example.valid?
-      example.errors[:description].size.should eq 1
+      expect(example.errors[:description].size).to eq 1
     end
 
     it 'has no error if everything is fine' do
       example.description = 'This is a nice foo thing'
       example.valid?
-      example.errors[:description].size.should eq 0
+      expect(example.errors[:description].size).to eq 0
     end
 
     it 'is valid if any language has a value' do
       example.description_translations = { en: '', de: '' }
       example.valid?
-      example.errors[:description].size.should eq 1
+      expect(example.errors[:description].size).to eq 1
 
       example.description_translations = { en: '', de: 'foobar' }
       example.valid?
-      example.errors[:description].size.should eq 0
+      expect(example.errors[:description].size).to eq 0
 
       example.description_translations = { en: 'foobar', de: nil }
       example.valid?
-      example.errors[:description].size.should eq 0
+      expect(example.errors[:description].size).to eq 0
     end
   end
 
@@ -40,60 +40,60 @@ describe SerialTranslator::SerialTranslatorValidator do
     it 'validates min length correctly' do
       example.title = '123'
       example.valid?
-      example.errors[:title].size.should eq 1
+      expect(example.errors[:title].size).to eq 1
     end
 
     it 'validates max length correctly' do
       example.title = 'f' * 26
       example.valid?
-      example.errors[:title].size.should eq 1
+      expect(example.errors[:title].size).to eq 1
     end
 
     it 'ignores blank fields' do
       example.title = ''
       example.valid?
-      example.errors[:title].size.should eq 0
+      expect(example.errors[:title].size).to eq 0
     end
 
     it 'validates all translation values' do
       example.title_translations = { en: '123', de: '123' }
       example.valid?
-      example.errors[:title].size.should eq 2
+      expect(example.errors[:title].size).to eq 2
 
       example.title_translations = { en: '123456', de: '123' }
       example.valid?
-      example.errors[:title].size.should eq 1
+      expect(example.errors[:title].size).to eq 1
 
       example.title_translations = { en: '123', de: '123456' }
       example.valid?
-      example.errors[:title].size.should eq 1
+      expect(example.errors[:title].size).to eq 1
 
       example.title_translations = { en: '123456', de: '123456' }
       example.valid?
-      example.errors[:title].size.should eq 0
+      expect(example.errors[:title].size).to eq 0
     end
 
     it 'validates correctly on language change as well' do
       I18n.locale = :de
       example.description = :something_to_make_it_pass_valid
       example.title = 'Valid foo'
-      example.should be_valid
+      expect(example).to be_valid
 
       I18n.locale = :en
       example.title = ''
-      example.should be_valid
+      expect(example).to be_valid
     end
   end
 
   describe '#kind' do
     it 'returns length if length validation is set' do
       validator = SerialTranslator::SerialTranslatorValidator.new(attributes: :foo, length: 1..2)
-      validator.kind.should eq :length
+      expect(validator.kind).to eq :length
     end
 
     it 'returns presence if it is no length validation' do
       validator = SerialTranslator::SerialTranslatorValidator.new(attributes: :foo)
-      validator.kind.should eq :presence
+      expect(validator.kind).to eq :presence
     end
   end
 end
--- spec/lib/serial_translator_spec.rb
+++ spec/lib/serial_translator_spec.rb
@@ -7,32 +7,32 @@ describe SerialTranslator do
   describe 'getting a translated value' do
     it 'returns the translated value in the requested locale' do
       object.title_translations = { de: 'German translation', en: 'English translation' }
-      object.title(:de).should eq 'German translation'
+      expect(object.title(:de)).to eq 'German translation'
     end
 
     it 'returns the origin locale value if no translation is present' do
       object.title_translations = { en: 'English translation' }
-      object.title(:de).should eq 'English translation'
+      expect(object.title(:de)).to eq 'English translation'
     end
 
     it 'returns the origin locale value if the translation is empty' do
       object.title_translations = { de: '', en: 'English translation' }
-      object.title(:de).should eq 'English translation'
+      expect(object.title(:de)).to eq 'English translation'
     end
 
     it 'returns nil if no translations are there at all' do
       object.title_translations = {}
-      object.title(:de).should be_nil
+      expect(object.title(:de)).to be_nil
     end
 
     it 'uses the given text locale' do
       object.title_translations = { de: 'Deutsch', en: 'English' }
       object.current_translation_locale = :en
-      object.title.should eq 'English'
+      expect(object.title).to eq 'English'
     end
 
     it 'does not fail even if translations were not initialized before' do
-      object.title.should be_blank
+      expect(object.title).to be_blank
     end
   end
 
@@ -40,74 +40,74 @@ describe SerialTranslator do
     it 'sets the value for the current text locale' do
       object.current_translation_locale = :de
       object.title = 'Deutsch'
-      object.title_translations.should eq({ de: 'Deutsch' })
+      expect(object.title_translations).to eq({ de: 'Deutsch' })
     end
 
     it 'nil removes the value for the current text locale' do
       object.title_translations = { de: 'Deutsch' }
       object.current_translation_locale = :de
       object.title = nil
-      object.title_translations.should eq({})
+      expect(object.title_translations).to eq({})
     end
 
     it 'blank string removes the value for the current text locale' do
       object.title_translations = { de: 'Deutsch' }
       object.current_translation_locale = :de
       object.title = "    "
-      object.title_translations.should eq({})
+      expect(object.title_translations).to eq({})
     end
   end
 
   describe 'getting an attribute for a specific locale' do
     it 'gets the correct translation' do
       object.title_translations = { de: 'Deutsch' }
-      object.title_de.should eq 'Deutsch'
+      expect(object.title_de).to eq 'Deutsch'
     end
 
     it 'uses fallback mechanisms as well' do
       object.title_translations = { en: 'English' }
-      object.title_de.should eq 'English'
+      expect(object.title_de).to eq 'English'
     end
   end
 
   describe 'writing an attribute with for a specific locale' do
     it 'sets the value for the correct locale' do
       object.title_de = 'Deutsch'
-      object.title_translations.should eq({ de: 'Deutsch' })
+      expect(object.title_translations).to eq({ de: 'Deutsch' })
     end
 
     it 'works for complex locales as well' do
       object.title_en_gb = 'Arrr'
-      object.title_translations.should eq({ :'en-GB' => 'Arrr' })
+      expect(object.title_translations).to eq({ :'en-GB' => 'Arrr' })
     end
   end
 
   describe '#current_translation_locale' do
     it 'returns the value that was given' do
       object.current_translation_locale = 'de'
-      object.current_translation_locale.should eq :de
+      expect(object.current_translation_locale).to eq :de
     end
 
     it 'must not return nil' do
       object.current_translation_locale = nil
-      object.current_translation_locale.should eq I18n.locale.to_sym
+      expect(object.current_translation_locale).to eq I18n.locale.to_sym
     end
   end
 
   describe '#translated_locales' do
     it 'returns an empty array if nothing was translated' do
-      object.translated_locales.should eq []
+      expect(object.translated_locales).to eq []
     end
 
     it 'includes all locales where something is present' do
       object.title_translations = { en: 'Foo', de: 'Foo' }
       object.summary_translations = { nb: 'Foo' }
-      object.translated_locales.should eq [:en, :de, :nb]
+      expect(object.translated_locales).to eq [:en, :de, :nb]
     end
 
     it 'does not include locales where all translations are blank' do
       object.title_translations = { en: 'Foo', de: '' }
-      object.translated_locales.should eq [:en]
+      expect(object.translated_locales).to eq [:en]
     end
   end
 
@@ -115,15 +115,15 @@ describe SerialTranslator do
     before { object.title_translations = { en: 'Foo', de: '' } }
 
     it 'returns true if there is a translation available for this locale' do
-      object.should be_translated_into :en
+      expect(object).to be_translated_into :en
     end
 
     it 'returns false if there is no translation present for this locale' do
-      object.should_not be_translated_into :de
+      expect(object).to_not be_translated_into :de
     end
 
     it 'returns false if this locale is unknown yet' do
-      object.should_not be_translated_into :yml
+      expect(object).to_not be_translated_into :yml
     end
   end
 end
